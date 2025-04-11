6efdcd42163d87fa310d100b29855b0cb8c5a935
Kimsor1111 <angkimsor@gmail.com>
2025-04-11 17:46:21 +0700

    add

--- src/component/Home/FeatureBrand.jsx
+++ src/component/Home/FeatureBrand.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FeatureBrand = ({ img }) => {
   return (
-    <div className="xl:w-[15.5%] lg:w-[28%] w-[49%] md:h-[120px] h-[60px] flex items-center justify-center rounded-md cursor-pointer bg-[#f8f8f8] p-10">
+    <div className="xl:w-[15.5%] lg:w-[28%] w-[49%] lg:grayscale-100 hover:grayscale-0 transition-all duration-150 md:h-[120px] h-[60px] flex items-center justify-center rounded-md cursor-pointer bg-[#f8f8f8] p-10">
       <img
         src={img}
         alt=""
