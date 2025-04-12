b85cec0a0029ae783f86933e03642200defaa69a
Kimsor1111 <angkimsor@gmail.com>
2025-04-12 22:07:20 +0700

    add the laoding naimation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="w-full h-[880px]  flex flex-col items-center justify-center">
+    <div className="w-full h-[480px]  flex flex-col items-center justify-center">
       <div className="loader animate-bounce"></div>
     </div>
   );
